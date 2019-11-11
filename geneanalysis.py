1874b1f24cb0dfae7d02520a230c92d1befbcd91
Venere <50956992+veneresabrina@users.noreply.github.com>
2019-11-11 18:40:38 +0100

    Add files via upload
    optimal dataframe

--- geneanalysis.py
+++ geneanalysis.py
@@ -5,14 +5,24 @@ Created on Wed Nov  6 15:16:29 2019
 
 @author: sabrina
 
-Discretizzare i segmenti di geni per capire quale valore attribuirgli
-Si crea un plot che mostra in blu i punti di start e in rosso di end
-Si valutano le eventuali sovrapposizioni
+Per il cromosoma1 si ricava un dataframe che ha come righe una discretizzazione
+delle coordinate del cromosoma e come colonne tutti i pazienti
+A ogni cella corrisponde il segment_mean più rappresentativo di quella porzione
+di cromosoma, se non ve n'è nessuno allora si assegna 0.
+Il valore più rappresentativo è quello che occupa la maggior parte del segmento
+
+I valori ottenuti non mi convincono. Perchè mette 0? Siamo sicuri che quella
+porzione non fosse rappresentata magari da un valore precedente o successivo?
+
+Rimane da definire il nome dei geni avendo le coordinate, alcune misure statistiche,
+eventuale dimensionality reduction, generalizzazione a tutti i cromosomi, salvando
+il dataset ottenuto sia per il kidney che per il lung tumor.
+
 """
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 kdata = pd.read_csv("kidney_data.csv")
 pat_names = kdata.GDC_Aliquot.unique()
@@ -22,26 +32,66 @@ for pat in pat_names:  # for each patient
     rows = kdata.loc[kdata[kdata.columns[0]] == pat]  #take all of patient1 rows
     # now get only chromosome 1
     rows = rows.loc[rows[rows.columns[1]] == '1']
-    rows = rows.drop(columns = [rows.columns[0],rows.columns[1],rows.columns[4]])
+    rows = rows.drop(columns = [rows.columns[1],rows.columns[4]])
     chr1 = chr1.append(rows)
     
 inizio = min(chr1["Start"])
 fine = max(chr1["End"])
 distanze = np.array(chr1["End"] - chr1["Start"])
-plt.hist(distanze, bins = 100)
-plt.title('Distribution of the dimensions of segments in CHR1')
-plt.xlabel('Length of the segment')
-plt.ylabel('Frequency')
-step = 2000000  # deciso dopo aver valutato l'istogramma
+hist, bin_edges = np.histogram(distanze, bins = len(chr1)/100)
+n = bin_edges[list(hist).index(max(hist)) + 1]
+s = str(int(n))
+step = int(s[0] + '0'*(len(s)-1))/2
 
-df = pd.DataFrame(index=range((fine-inizio)/step+1),columns = list(pat_names))
-#for p in range(inizio, fine, passo):
-#BO vedere quaderno
-if chr1["Start"][j] >= inizio and chr1["End"][j] <= fine:
-    
-    
-    
-    
+#y, x, _ = plt.hist(distanze, bins = 77)
+#plt.title('Distribution of the dimensions of segments in CHR1')
+#plt.xlabel('Length of the segment')
+#plt.ylabel('Frequency')
+#print x.max()
+#print y.max()
+#step = 2000000  # deciso dopo aver valutato l'istogramma
+
+
+# redefine index to match segments points 
+l1 =  list(range(inizio, fine - step, step))
+l2 = list(range(inizio + step, fine, step))
+
+df = pd.DataFrame(columns = list(pat_names))
+df.insert(0,"Min",l1)
+df.insert(1,"Max",l2)
+df = df.set_index(['Min', 'Max'])
+
+# per ogni riga di chr1
+for index, segment in chr1.iterrows():
+    # per ogni range predefinito
+    for min_max in df.index:
+        # se non è ancora stato messo un valore
+        if df.isnull().at[min_max, segment["GDC_Aliquot"]]:
+            # se cade all'interno dei range predefiniti e occuperebbe almeno il 60% del segmento
+            if min_max[0] >= segment["Start"] and min_max[1] <= segment["End"] and segment["End"] - segment["Start"] >= 0.6 * step:
+                df.at[min_max, segment["GDC_Aliquot"]] = segment['Segment_mean']
+            # se è a cavallo tra due range predefiniti ma molto vicino a quello corrente
+            elif (min_max[0] - segment["Start"] < 0.01*step or min_max[1] - segment["End"] < 0.01*step) and segment["End"] - segment["Start"] >= 0.6 * step:
+                df.at[min_max, segment["GDC_Aliquot"]] = segment['Segment_mean']
+            # se è troppo piccolo rispetto al range predefinito
+            elif segment["End"] - segment["Start"] < 0.6 * step:
+                df.at[min_max, segment["GDC_Aliquot"]] = 0
+        # se gli è già stato assegnato un valore ma il nuovo segmento è predominante
+        elif min_max[0] >= segment["Start"] and min_max[1] <= segment["End"] and segment["End"] - segment["Start"] >= 0.6 * step:
+            df.at[min_max, segment["GDC_Aliquot"]] = segment['Segment_mean']
+
+
+
+#
+## itera su ogni riga del chr1
+#for index, segment in chr1.iterrows():
+#    for ind in df.index:
+#        if ind >= segment["Start"] and ind <= segment["End"]:
+#            df.at[ind, df.columns == segment["GDC_Aliquot"]] = segment['Segment_mean']
+#        # Se non rientra nei range definiti dallo step allora allarga leggermente il range
+#        elif ind >= segment["Start"] - 1000 and ind <= segment["End"] + 1000:
+#            df.at[ind, df.columns == segment["GDC_Aliquot"]] = segment['Segment_mean']
+#            
 
 #    # l'idea era di creare la discretizzazione a partire dai reali segmenti
 #    # a quel punto valutare caso per caso quale valore attribuire
